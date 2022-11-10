f9457515114d7534435fc1c300338455efa0ecd3
bookgvi <bookgvi@gmail.com>
2022-11-10 18:52:23 +0300

    git init + di + ioc

---
+++ src/main/java/SpringQuickly/singletone/main/Application.java
@@ -0,0 +1,38 @@
+package SpringQuickly.singletone.main;
+
+import SpringQuickly.singletone.config.ProjectConfig;
+import SpringQuickly.singletone.service.PushServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.concurrent.ExecutionException;
+import java.util.logging.Logger;
+
+public class Application {
+    private static Logger logger = Logger.getLogger(Application.class.getName());
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        logger.info("Context created:\n\t" + ctx.getDisplayName());
+
+//        IService mailService = ctx.getBean(MailServiceImpl.class);
+
+
+        PushServiceImpl pushServiceSingle1 = ctx.getBean("pushSingletone1", PushServiceImpl.class);
+        PushServiceImpl pushServiceSingle2 = ctx.getBean("pushSingletone2", PushServiceImpl.class);
+        PushServiceImpl pushServiceSingletoneScope1 = ctx.getBean("pushServiceSingletoneScope1", PushServiceImpl.class);
+        PushServiceImpl pushServiceSingletoneScope2 = ctx.getBean("pushServiceSingletoneScope2", PushServiceImpl.class);
+        logger.info("PushServiceBean1:\n\t" + pushServiceSingle1.getClass().getSimpleName());
+        logger.info("PushServiceBean2:\n\t" + pushServiceSingle1.getClass().getName());
+        logger.info("pushServiceSingletoneScope1:\n\t" + pushServiceSingletoneScope1.getClass().getName());
+        logger.info("pushServiceSingletoneScope2:\n\t" + pushServiceSingletoneScope2.getClass().getName());
+
+        pushServiceSingle1.setChf("1");
+        pushServiceSingletoneScope1.setChf("2");
+        System.out.println(pushServiceSingle1.getChf());
+        System.out.println(pushServiceSingle2.getChf());
+        System.out.println(pushServiceSingletoneScope1.getChf());
+        System.out.println(pushServiceSingletoneScope2.getChf());
+
+    }
+}
---
+++ src/main/java/SpringQuickly/singletone/service/PushServiceImpl.java
@@ -0,0 +1,39 @@
+package SpringQuickly.singletone.service;
+
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+
+@Service("pushServiceSingletoneScope1")
+@Lazy
+public class PushServiceImpl implements IService {
+    public static IService INSTANCE = getINSTANCE();
+    private String chf = "QQQ";
+
+    public  PushServiceImpl() {}
+
+    private static IService getINSTANCE() {
+        IService instance = null;
+        if (INSTANCE == null) {
+            synchronized (PushServiceImpl.class) {
+                if (INSTANCE == null) {
+                    instance = new PushServiceImpl();
+                }
+            }
+        }
+        return instance;
+    }
+
+    @Override
+    public void send(String text) {
+        System.out.printf("\"%s\" has been pushed\n", text);
+    }
+
+    public String getChf() {
+        return chf;
+    }
+
+    public void setChf(String chf) {
+        this.chf = chf;
+    }
+}
